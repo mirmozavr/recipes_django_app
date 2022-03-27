f588a7e4129b87edcdad45b9425b79db057d9db5
Mo <mirmozavr@gmail.com>
2022-03-27 22:55:17 +0300

    Add view function test

---
+++ recepies/tests.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from django.test import TestCase
+
+from .models import Food, Weight, Recipe
+from .views import dish_calories_per100g
+
+
+class TestDishCaloriesPer100G(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        for data in (1, 'Food1', 10), (2, 'Food2', 30), (3, 'Food3', 50):
+            Food(*data).save()
+
+        Recipe(
+            1, 'Recipe1', 'Text', 'other', 'recipe1', timedelta(), datetime.now()
+        ).save()
+
+        Recipe(
+            2, 'Recipe2', 'Text', 'other', 'recipe2', timedelta(), datetime.now()
+        ).save()
+        for data in (1, 1, 1, 100), (2, 1, 2, 400), (3, 1, 3, 300):
+            Weight(*data).save()
+
+    def test_simple_recipe_average_calorie(self):
+        self.assertEqual(dish_calories_per100g('recipe1'), 35)
+
+    def test_empty_recipe_returns_hyphen(self):
+        self.assertEqual(dish_calories_per100g('recipe2'), '-')
